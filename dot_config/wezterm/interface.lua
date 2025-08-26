a477e49d0b06139b7981ec1a5580a9843e45add2
Joel Baxter <joel.baxter@neogeographica.com>
2025-08-25 19:17:46 -0700

    add my PROMPT_EOL_MARK character to selection word boundary

--- dot_config/wezterm/interface.lua
+++ dot_config/wezterm/interface.lua
@@ -1,10 +1,12 @@
--- Customize behavior for the windowing interface.
+-- Customize behavior for the selection and windowing interfaces.
 
 local wezterm = require 'wezterm'
 
 local module = {}
 
 function module.configure(config)
+  -- Add my custom PROMPT_EOL_MARK character to selection word boundary.
+  config.selection_word_boundary = " \t\n{}[]()\"'`" .. utf8.char(0xf04d)
   -- Disable "are you sure" prompt when closing tab.
   -- XXX Trying to disable tab close confirmation using all the mechanisms
   -- below. This works for the ctrl-shift-w key shortcut, and also works for
